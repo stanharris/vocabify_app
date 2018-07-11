dc8e2125e20e0f8ad94bb4445b08e6e70073065d
Paul Breslin <paul.breslin@deliveroo.co.uk>
2018-07-12 00:48:21 +0100

    Render List items on Settings page

--- src/components/SettingsView/index.js
+++ src/components/SettingsView/index.js
@@ -4,14 +4,46 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 import Card from '../Card';
+import List from '../List';
+import ListItem from '../ListItem';
 import './styles.css';
 
 class SettingsView extends Component<{}> {
+  state = {
+    definitionSources: [
+      {
+        id: 'mock-id',
+        name: 'Oxford Dictionary',
+        enabled: true
+      },
+      {
+        id: 'mock-other-id',
+        name: 'Cambridge Dictionary',
+        enabled: true
+      }
+    ]
+  };
+
+  toggleSource = (id: string, enabled: boolean) => {
+    // Update firestore
+  };
+
+  renderDefinitionSources = () =>
+    this.state.definitionSources.map(item => (
+      <ListItem
+        id={item.id}
+        name={item.name}
+        enabled={item.enabled}
+        onClick={this.toggleSource}
+      />
+    ));
+
   render() {
     return (
       <div className="settings-view">
         <Card>
           <h3>Settings</h3>
+          <List>{this.renderDefinitionSources()}</List>
         </Card>
       </div>
     );
