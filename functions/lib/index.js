4559cd863bfd3ba338cc5fe5fb66fd22187d35c7
Paul Breslin <paul.breslin@deliveroo.co.uk>
2018-07-31 01:31:48 +0100

    Add compiled functions lib

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -10,6 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const node_fetch_1 = require("node-fetch");
+const _ = require("lodash");
 exports.wordsAPI = functions.https.onCall((data, context) => new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
     const headers = new node_fetch_1.Headers();
     headers.append('X-Mashape-Key', functions.config().mashape.key);
@@ -29,10 +30,49 @@ exports.wordsAPI = functions.https.onCall((data, context) => new Promise((resolv
                     definition,
                     example: hasExample ? examples[0] : null,
                     partOfSpeech: hasPartOfSpeech ? partOfSpeech : null,
-                    enabled: index < 3
+                    enabled: index === 0
                 };
             })
             : null);
+    }
+    catch (error) {
+        reject(error);
+        // TODO - Log error
+    }
+})));
+exports.oxford = functions.https.onCall((data, context) => new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+    const headers = new node_fetch_1.Headers();
+    headers.append('app_id', functions.config().oxford.app_id);
+    headers.append('app_key', functions.config().oxford.app_key);
+    try {
+        const request = yield node_fetch_1.default(`https://od-api.oxforddictionaries.com:443/api/v1/entries/en/${data.word}`, { headers });
+        const response = yield request.json();
+        const wordData = response.results.map(result => result.lexicalEntries.map(lexicalEntry => {
+            const filteredList = lexicalEntry.entries[0].senses.filter(sense => Boolean(sense.definitions));
+            const formattedList = filteredList.map(sense => {
+                const { definitions, examples } = sense;
+                const hasDefinition = Boolean(definitions);
+                const hasExample = Boolean(examples);
+                return hasDefinition
+                    ? {
+                        definition: definitions[0],
+                        example: hasExample ? examples[0].text : null
+                    }
+                    : null;
+            });
+            return formattedList;
+        }));
+        const flattenedList = _.flattenDeep(wordData);
+        const hasDefinitions = Boolean(flattenedList.length);
+        if (hasDefinitions) {
+            // Enable first definition
+            resolve(flattenedList.map((item, index) => {
+                return Object.assign({}, item, { enabled: index === 0 });
+            }));
+        }
+        else {
+            resolve(null);
+        }
     }
     catch (error) {
         reject(error);
