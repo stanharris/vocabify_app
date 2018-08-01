7d6718c102d7de4984c40e8507ce49755f15504d
Paul Breslin <paul.breslin@deliveroo.co.uk>
2018-08-01 09:58:11 +0100

    Move AppFooter to css modules

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,12 +1,12 @@
 // @flow
 import React, { PureComponent } from 'react';
 
-import './styles.css';
+import styles from './styles.module.css';
 
 class AppFooter extends PureComponent<{}> {
   render() {
     return (
-      <div className="app-footer">
+      <div className={styles.appFooter}>
         <a
           href="https://github.com/paulbreslin/vocabify_app"
           target="_blank"
